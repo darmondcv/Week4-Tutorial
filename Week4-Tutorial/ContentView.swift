2d0f0d0cf9a3f425caf5a66532ce2987fde2a66d
Dang <darmondcv@gmail.com>
2024-07-29 15:03:39 +0700

    Initial Commit

---
+++ Week4-Tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week4-Tutorial
+//
+//  Created by Darmond on 29/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Week4-Tutorial/Week4_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week4_TutorialApp.swift
+//  Week4-Tutorial
+//
+//  Created by Darmond on 29/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Week4_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
